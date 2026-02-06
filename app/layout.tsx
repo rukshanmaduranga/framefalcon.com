125b2fa15116fbd8a5ecba80b706662662bda5c4
Rukshan Maduranga <rukshanmweb@gmail.com>
2026-02-07 00:56:35 +0530

    Set dark mode as default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-navy-blue text-gray-900 dark:text-white`}
       >
